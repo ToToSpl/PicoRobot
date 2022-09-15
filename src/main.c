3866be9a08e3464e16b72ea470f68eff8e932763
Jacek Grzybowski <totospl99@gmail.com>
2022-09-15 20:51:07 +0200

    encoder readout

--- src/main.c
+++ src/main.c
@@ -1,14 +1,40 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 
+#define MOTOR_LEFT_PIN_A 16
+#define MOTOR_LEFT_PIN_B 17
+#define MOTOR_LEFT_ENCODER_1 18
+#define MOTOR_LEFT_ENCODER_2 19
 
 int main() {
-  const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-  gpio_init(LED_PIN);
-  gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  stdio_init_all();
+
+  gpio_init(MOTOR_LEFT_ENCODER_1);
+  gpio_set_dir(MOTOR_LEFT_ENCODER_1, GPIO_IN);
+  gpio_pull_down(MOTOR_LEFT_ENCODER_1);
+
+  gpio_init(MOTOR_LEFT_ENCODER_2);
+  gpio_set_dir(MOTOR_LEFT_ENCODER_2, GPIO_IN);
+  gpio_pull_down(MOTOR_LEFT_ENCODER_2);
+
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+  bool encoder1, encoder2;
+  bool led_state = false;
   while (true) {
-    gpio_put(LED_PIN, 1);
+
+    encoder1 = gpio_get(MOTOR_LEFT_ENCODER_1);
+    encoder2 = gpio_get(MOTOR_LEFT_ENCODER_2);
+
+    printf("%d\t%d\n", encoder1, encoder2);
+
+    gpio_put(PICO_DEFAULT_LED_PIN, led_state);
+    led_state = !led_state;
+
     sleep_ms(500);
-    gpio_put(LED_PIN, 0);
-    sleep_ms(250);
   }
+
+  return 0;
 }
