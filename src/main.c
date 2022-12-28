5e553d77d0109f0ba6da6461091b406ace1ee734
Jacek Grzybowski <totospl99@gmail.com>
2022-12-28 20:50:17 +0100

    correct pinout and sensor setup

--- src/main.c
+++ src/main.c
@@ -5,62 +5,72 @@
 #include "encoder.h"
 #include "motor_controller.h"
 #include "optical_sensor.h"
-
-#define MOTOR_LEFT_PIN_A 16
-#define MOTOR_LEFT_PIN_B 17
-#define MOTOR_LEFT_ENCODER_1 18
-#define MOTOR_LEFT_ENCODER_2 19
-
+#include "pinout.h"
 
 int main() {
 
   stdio_init_all();
-  adc_init();
 
-  // struct OpticalSensor bumperL;
-
-  // optical_sensor_init(&bumperL, GPIO_ADC_FIRST, BUMPER_SENS_THRESH, true);
-  
-  struct BumperSensor bumperL;
-  bumper_sensor_init(&bumperL, 26, 0);
-
-  struct Encoder encoder;
+  // bumpers
+  BumperSensor bumperL, bumperR;
+  bumper_sensor_init(&bumperL, BUMPER_LEFT, 0);
+  bumper_sensor_init(&bumperR, BUMPER_RIGHT, 0);
+  // floor sensors (set as digital sensors)
+  BumperSensor floorL1, floorL2, floorR1, floorR2;
+  bumper_sensor_init(&floorL1, FLOOR_S_L1, 0);
+  bumper_sensor_init(&floorL2, FLOOR_S_L2, 0);
+  bumper_sensor_init(&floorR1, FLOOR_S_R1, 0);
+  bumper_sensor_init(&floorR2, FLOOR_S_R2, 0);
 
   uint pio_offset = encoder_create_pio_program(pio0);
-  encoder_init(&encoder, pio0, 0, MOTOR_LEFT_ENCODER_1, 0, pio_offset);
+  Encoder encoderL, encoderR;
+  encoder_init(&encoderL, pio0, 1, MOTOR_LEFT_ENCODER_1, 0, pio_offset);
+  encoder_init(&encoderR, pio0, 0, MOTOR_RIGHT_ENCODER_1, 1, pio_offset);
 
-  struct MotorController motor_left;
-  motor_contr_init(&motor_left, &encoder, MOTOR_LEFT_PIN_A, MOTOR_LEFT_PIN_B,
-                   1); // direction reversed due to wiring
+  MotorController motor_left, motor_right;
+  motor_contr_init(&motor_left, &encoderL, MOTOR_LEFT_PIN_A, MOTOR_LEFT_PIN_B,
+                   1);
 
+  motor_contr_init(&motor_right, &encoderR, MOTOR_RIGHT_PIN_A,
+                   MOTOR_RIGHT_PIN_B, 1);
   uint16_t cnt = 0;
   float dir = 0.01f;
   float spd = 0.0f;
   motor_contr_set_spd(&motor_left, spd);
+  motor_contr_set_spd(&motor_right, spd);
 
   while (1) {
-    encoder_update(&encoder);
+    encoder_update(&encoderL);
+    encoder_update(&encoderR);
 
-    // bool trig = optical_sensor_measure(&bumperL);
-    bool trig = bumper_sensor_measure(&bumperL);
-    if (trig)
-      printf("Bumped!\n");
-    else
-      printf("Not bumped.\n");
+    printf("\n\
+        BUMPER_L:\t%d\n\
+        BUMPER_R:\t%d\n\
+        FLOOR_L1:\t%d\n\
+        FLOOR_L2:\t%d\n\
+        FLOOR_R2:\t%d\n\
+        FLOOR_R1:\t%d\n\
+        ENCODER_L:\t%f\n\
+        ENCODER_R:\t%f\n\
+        \n",
+           bumper_sensor_measure(&bumperL), bumper_sensor_measure(&bumperR),
+           bumper_sensor_measure(&floorL1), bumper_sensor_measure(&floorL2),
+           bumper_sensor_measure(&floorR2), bumper_sensor_measure(&floorR1),
+           encoderL.distance, encoderR.distance);
 
-    /*
     if (cnt >= 30) {
       cnt = 0;
       if (spd < -0.25f || spd > 0.25f) {
         dir = -dir;
       }
       spd += dir;
-      motor_contr_set_spd(&motor_left, spd);
+      motor_contr_set_spd(&motor_left, -spd);
+      motor_contr_set_spd(&motor_right, spd);
     }
     cnt++;
+
     motor_contr_update(&motor_left);
-    printf("%.8f %.8f %.8f\n", spd, encoder.speed, motor_left.err_prev);
-    */
+    motor_contr_update(&motor_right);
 
     sleep_ms(10);
   }
---
+++ src/pinout.h
@@ -0,0 +1,22 @@
+#ifndef ROBOT_PINOUT
+#define ROBOT_PINOUT
+
+#define MOTOR_LEFT_PIN_A 10
+#define MOTOR_LEFT_PIN_B 11
+#define MOTOR_LEFT_ENCODER_1 18
+#define MOTOR_LEFT_ENCODER_2 19
+
+#define MOTOR_RIGHT_PIN_A 12
+#define MOTOR_RIGHT_PIN_B 13
+#define MOTOR_RIGHT_ENCODER_1 20
+#define MOTOR_RIGHT_ENCODER_2 21
+
+#define BUMPER_LEFT 6
+#define BUMPER_RIGHT 7
+
+#define FLOOR_S_L1 3
+#define FLOOR_S_L2 5
+#define FLOOR_S_R1 2
+#define FLOOR_S_R2 4
+
+#endif
