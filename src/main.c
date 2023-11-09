15c411ca91bfb2a5c0b6ef56ed0bf8226a94b99a
Jacek Grzybowski <totospl99@gmail.com>
2023-11-10 00:36:27 +0100

    second core usage

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "hardware/pwm.h"
 #include "hardware/uart.h"
+#include "pico/multicore.h"
 #include "pico/stdio.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
@@ -9,23 +10,45 @@
 #include "optical_sensor.h"
 #include "pinout.h"
 
+BumperSensor bumperL, bumperR;
+BumperSensor floorL1, floorL2, floorR1, floorR2;
+Encoder encoderL, encoderR;
+
+void main_core1() {
+  while (1) {
+
+    printf("{\
+              BUMPER_L: %d,\
+              BUMPER_R: %d,\
+              FLOOR_L1: %d,\
+              FLOOR_L2: %d,\
+              FLOOR_R2: %d,\
+              FLOOR_R1: %d,\
+              ENCODER_L: %f,\
+              ENCODER_R: %f\
+            }\n",
+           bumper_sensor_measure(&bumperL), bumper_sensor_measure(&bumperR),
+           bumper_sensor_measure(&floorL1), bumper_sensor_measure(&floorL2),
+           bumper_sensor_measure(&floorR2), bumper_sensor_measure(&floorR1),
+           encoderL.distance, encoderR.distance);
+    sleep_ms(10);
+  }
+}
+
 int main() {
 
   stdio_init_all();
 
   // bumpers
-  BumperSensor bumperL, bumperR;
   bumper_sensor_init(&bumperL, BUMPER_LEFT, 0);
   bumper_sensor_init(&bumperR, BUMPER_RIGHT, 0);
   // floor sensors (set as digital sensors)
-  BumperSensor floorL1, floorL2, floorR1, floorR2;
   bumper_sensor_init(&floorL1, FLOOR_S_L1, 0);
   bumper_sensor_init(&floorL2, FLOOR_S_L2, 0);
   bumper_sensor_init(&floorR1, FLOOR_S_R1, 0);
   bumper_sensor_init(&floorR2, FLOOR_S_R2, 0);
 
   uint pio_offset = encoder_create_pio_program(pio0);
-  Encoder encoderL, encoderR;
   encoder_init(&encoderL, pio0, 1, MOTOR_LEFT_ENCODER_1, 0, pio_offset);
   encoder_init(&encoderR, pio0, 0, MOTOR_RIGHT_ENCODER_1, 1, pio_offset);
 
@@ -35,35 +58,18 @@ int main() {
 
   motor_ctrl_init(&motor_right, &encoderR, MOTOR_RIGHT_PIN_A, MOTOR_RIGHT_PIN_B,
                   1);
+
+  multicore_launch_core1(main_core1);
+
   uint16_t cnt = 0;
   float dir = 0.01f;
   float spd = 0.0f;
   motor_ctrl_set_spd(&motor_left, spd);
   motor_ctrl_set_spd(&motor_right, spd);
 
-  char *data = "Hello!";
-
   while (1) {
     encoder_update(&encoderL);
     encoder_update(&encoderR);
-
-    // uart_write_blocking(uart0, data, 8);
-
-    puts_raw("Hello world");
-    printf("\n\
-        BUMPER_L:\t%d\n\
-        BUMPER_R:\t%d\n\
-        FLOOR_L1:\t%d\n\
-        FLOOR_L2:\t%d\n\
-        FLOOR_R2:\t%d\n\
-        FLOOR_R1:\t%d\n\
-        ENCODER_L:\t%f\n\
-        ENCODER_R:\t%f\n\
-        \n",
-           bumper_sensor_measure(&bumperL), bumper_sensor_measure(&bumperR),
-           bumper_sensor_measure(&floorL1), bumper_sensor_measure(&floorL2),
-           bumper_sensor_measure(&floorR2), bumper_sensor_measure(&floorR1),
-           encoderL.distance, encoderR.distance);
 
     if (cnt >= 30) {
       cnt = 0;
